b9797269a7a92b7cc15f62b4b03cf949bf8ac5f7
Petarpg <petarivvvvv@gmail.com>
2025-04-11 14:38:43 +0300

    Github-deploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
-  build: {
-    outDir: 'dist',
-  },
-}); 
+  base: "/Landing-page-KD/", // Задай базовия път спрямо името на репото
+});
