8e57e84d68c612cb552e3d44e1244bb714e309a0
Petarpg <petarivvvvv@gmail.com>
2025-04-26 14:57:15 +0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  base: "/Landing-page-KD/", // <-- THIS IS THE IMPORTANT PART
-  plugins: [react()],
-});
